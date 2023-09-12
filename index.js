f25e5071adaceea8b9f290532161263a9fc1d428
Kassim Zaid <81573513+kzaid0767@users.noreply.github.com>
2023-09-12 16:22:19 -0400

    added event listening to plus and minus buttons

--- index.js
+++ index.js
@@ -2,7 +2,47 @@ import { menuArray } from "./data.js";
 
 /* consts html elemens */
 const mainEl = document.querySelector('#main-app')
-// document.getElementById('shadow-host-companion').style.border = 'none'
+
+/* item ordered count */
+
+let pizzaCount = 0
+let burgerCount = 0
+let shakeCount = 0
+let totalCount = pizzaCount + burgerCount + shakeCount
+
+document.addEventListener('click', handleClick)
+
+function handleClick(e){
+    if(e.target.dataset.add || e.target.dataset.minus){
+        changeItemCount(e.target.dataset)
+    }
+}
+
+/* handling adding, subtracting, removing */
+
+function changeItemCount(value) {
+    if(value.add){
+        let id = parseInt(value.add)
+        if(id===0){
+            pizzaCount++
+        } else if(id===1){
+            burgerCount++
+        } else {
+            shakeCount++
+        }
+    } else {
+        let id = parseInt(value.minus)
+        if(id===0){
+            pizzaCount > 0? pizzaCount-- : 0
+        } else if(id===1){
+            burgerCount > 0? burgerCount-- :0
+        } else {
+            shakeCount > 0 ? shakeCount-- : 0
+        }
+    }
+
+    
+}
 
 
 function getAppHtml(){
@@ -21,8 +61,8 @@ function getAppHtml(){
                             <h3>$${item.price}</h3>
                         </div>
                         <div class="container-add-minus">
-                            <p class="add-minus">+</p>
-                            <p class="add-minus">-</p>
+                            <p class="add-minus" data-add="${item.id}">+</p>
+                            <p class="add-minus" data-minus="${item.id}">-</p>
                         </div>
                     </div>
                 </div>`
